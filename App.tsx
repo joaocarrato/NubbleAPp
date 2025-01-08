810ea0714c826c1c7a8f4a78e2912618ee8118d1
joaocarrato <joaocarrato18@gmail.com>
2025-01-07 23:13:26 -0300

    feat: add useAppTheme hook and integrate SVG icons for improved theming support

--- App.tsx
+++ App.tsx
@@ -3,7 +3,10 @@ import { SafeAreaView, View } from 'react-native';
 import { Text } from './src/components/Text/Text';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
-import { Button } from './src/components/Button/Button';
+import { EyeOffIcon } from './src/assets/icons/EyeOffIcon';
+import { EyeOnIcon } from './src/assets/icons/EyeOnIcon';
+import { Box } from './src/components/Box/Box';
+import { Icon } from './src/components/Icon/Icon';
 
 function App(): React.JSX.Element {
   return (
@@ -12,10 +15,9 @@ function App(): React.JSX.Element {
         <SafeAreaView>
           <Text preset="headingLarge">Coffstack</Text>
 
-          <Button title="Primary" mb="s12" />
-          <Button disabled preset="outline" title="Outline" mb="s12" />
-
-          <Button title="loading" loading />
+          <Box flexDirection="row" gap="s8">
+            <Icon name="eyeOn" color="carrotSecondary" size={40} />
+          </Box>
         </SafeAreaView>
       </View>
     </ThemeProvider>
--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -3,14 +3,14 @@ import {
   ActivityIndicatorProps,
   ActivityIndicator as RNActivityIndicator,
 } from 'react-native';
-import { Theme, ThemeColors } from '../../theme/theme';
-import { useTheme } from '@shopify/restyle';
+import { ThemeColors } from '../../theme/theme';
+import { useAppTheme } from '../../hooks/useAppTheme';
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
   color: ThemeColors;
 }
 
 export function ActivityIndicator({ color, ...activityIndicator }: Props) {
-  const { colors } = useTheme<Theme>();
+  const { colors } = useAppTheme();
   return <RNActivityIndicator {...activityIndicator} color={colors[color]} />;
 }
